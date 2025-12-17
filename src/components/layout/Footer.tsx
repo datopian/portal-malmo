61558f20e8dc7c67fcf5ea2db9205e72cbcfc4d6
William Lima <willy.lima89@gmail.com>
2025-12-17 10:11:02 -0100

    update footer content

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,88 +1,69 @@
 import { getTranslations } from "next-intl/server";
 import Container from "../ui/container";
-import { Linkedin } from "lucide-react";
+import Link from "next/link";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700", "900"],
+  subsets: ["latin"],
+});
 
 export default async function Footer() {
   const t = await getTranslations();
 
-  const footerLinks = [
-    {
-      title: t("Common.aboutUs"),
-      items: [
-        {
-          label: t("Footer.whatIsOpenData1"),
-          href: "#",
-        },
-        {
-          label: t("Footer.requestData"),
-          href: "#",
-        },
-        {
-          label: t("Footer.privacyPolicy"),
-          href: "#",
-        },
-      ],
-    },
-    {
-      title: t("Footer.contactTitle"),
-      items: [
-        {
-          label: "info@opendata.se", // usually not translated
-          href: "mailto:info@opendata.se",
-          className: "break-all",
-        },
-        {
-          label: t("Footer.newsletter"),
-          href: "#",
-        },
-        {
-          label: t("Footer.address"),
-          isText: true,
-          className: "whitespace-pre-line",
-        },
-        {
-          label: t("Footer.accessibilityStatement"),
-          href: "#",
-        },
-        {
-          label: "LinkedIn",
-          href: "#",
-          icon: <Linkedin width={18} />,
-          className: "flex items-center gap-1",
-        },
-      ],
-    },
-  ];
-
   return (
     <footer className="py-10 bg-theme-green text-theme-green-foreground">
-      <Container className="grid gap-8 md:grid-cols-3">
-        {footerLinks.map((section) => (
-          <div key={section.title}>
-            <h2 className="text-2xl font-bold mb-4">{section.title}</h2>
-            <ul className="space-y-3">
-              {section.items.map((item) => (
-                <li key={item.label}>
-                  {item.href ? (
-                    <a
-                      href={item.href}
-                      className={`hover:underline ${item.className ?? ""}`}
-                    >
-                      {item.icon}
-                      {item.icon ? <span>{item.label}</span> : item.label}
-                    </a>
-                  ) : (
-                    <p className={item.className ?? ""}>{item.label}</p>
-                  )}
-                </li>
-              ))}
-            </ul>
+      <Container className="grid gap-10 lg:gap-20 grid-2 lg:grid-cols-3">
+        <div className="whitespace-pre-line">{t("Footer.note")}</div>
+        <div>
+          <h2 className="text-2xl font-bold mb-4">
+            {t("Footer.contactTitle")}
+          </h2>
+          <ul className="space-y-3">
+            <li>
+              <span className="font-semibold">{t("Common.email")}: </span>
+              <Link
+                href="mailto:malmostad@malmo.se"
+                className="hover:underline"
+              >
+                malmostad@malmo.se
+              </Link>
+            </li>
+
+            <li>
+              <span className="font-semibold">{t("Common.address")}: </span>
+              Malmö stad, 250 80 Malmö
+            </li>
+            <li>
+              <span className="font-semibold">{t("Common.phone")}: </span>
+              <Link
+                href="tel:+46 40 341000"
+                className="hover:underline"
+              >
+                +46 40 341000
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="md:text-right space-y-1 text-sm flex flex-col">
+          <div className="flex items-center gap-1 md:justify-end">
+            <span className="text-sm">{t("Footer.builtWith")}</span>
+            <a
+              className="flex flex-col md:flex-row items-center justify-end gap-1 "
+              href="https://portaljs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span
+                className={`font-extrabold text-white text-xl sm:text-lg ${roboto.className}`}
+              >
+                🌀 PortalJS
+              </span>
+            </a>
+            <span>{t("Footer.onCKAN")}</span>
           </div>
-        ))}
-        <div className="md:text-right space-y-3 text-sm">
-          <div className="w-[180px] h-[64px] bg-white ml-auto" />
-          <p>{t("Footer.builtWithPortaljs")}</p>
-          <p className="mt-2">{t("Footer.exportCatalog")}</p>
+          <p className="">{t("Footer.exportCatalog")}</p>
         </div>
       </Container>
     </footer>
