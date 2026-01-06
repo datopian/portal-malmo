55a8c8f8da4b6e35bdc6f2b7233262646d829fd3
William Lima <willy.lima89@gmail.com>
2026-01-06 22:28:57 -0100

    fix org validation, remove orgs page

--- src/app/[locale]/organizations/loading.tsx
+++
@@ -1,9 +0,0 @@
-import PageLoading from "@/components/layout/PageLoading"
-import React from "react"
-
-
-export default function PageSkeleton() {
-  return (
-    <PageLoading type="grid"/>
-  )
-}
