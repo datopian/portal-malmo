17caaae0d4eb62fcc8bf19b95d87008a5e28b707
William Lima <willy.lima89@gmail.com>
2026-01-22 21:10:49 -0100

    docs: add documentation

---
+++ docs/banner/README.md
@@ -0,0 +1,20 @@
+# Disclaimer banner
+
+Adding a dismissable banner on top of all pages, with translatable markdown content.
+
+### How to manage the content
+
+To **enable** the disclaimer banner:
+
+* Navigate to `/content/banner` in your repository
+* Add the English content to `en.md`
+* Add the Swedish content to `sv.md`
+* Commit your changes to `main` (production) or `staging` (staging environment)
+
+Note that markdown is supported.
+
+To **disable** the banner, simply clear the contents of the files.
+
+Screenshot:
+![alt text](disclaimer-banner.png)
+
--- src/components/layout/DisclaimerBanner.tsx
+++ src/components/layout/DisclaimerBanner.tsx
@@ -1,37 +1,29 @@
 "use client";
 
 import { X } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Container from "../ui/container";
 import MarkdownRender from "../ui/markdown";
 
-const STORAGE_KEY = "disclaimerBannerDismissed";
 
 export default function DisclaimerBanner({
   content = "",
 }: {
   content: string | null;
 }) {
-  const [show, setShow] = useState<boolean | null>(null);
-
-
-  useEffect(() => {
-    const dismissed = localStorage.getItem(STORAGE_KEY);
-    setShow(!dismissed);
-  }, []);
+  const [show, setShow] = useState<boolean | null>(true);
 
   const handleDismiss = () => {
-    localStorage.setItem(STORAGE_KEY, "true");
     setShow(false);
   };
 
-  if (show === null || !show) return null;
+  if (!show) return null;
 
   return (
     content && (
       <div className="bg-gray-900 py-2.5 ">
         <Container>
-          <div className="flex items-center gap-x-6 bg-gray-900 ">
+          <div className="flex gap-x-6 bg-gray-900 ">
             <div className="block">
               <div
                 className="text-white text-sm/6"
@@ -40,10 +32,10 @@ export default function DisclaimerBanner({
               </div>
             </div>
 
-            <div className="flex flex-1 justify-end">
+            <div className="flex flex-1 h-fit w-fit  justify-end">
               <button
                 type="button"
-                className="-m-3 p-3 focus-visible:-outline-offset-4 text-white cursor-pointer"
+                className=" p-3 focus-visible:-outline-offset-4 text-white cursor-pointer"
                 onClick={handleDismiss}
               >
                 <span className="sr-only">Dismiss</span>
