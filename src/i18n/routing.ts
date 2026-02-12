45e8f7c9d63f454d79255c9b824b7e0a15d4827f
William Lima <willy.lima89@gmail.com>
2026-02-12 17:00:22 -0100

    chore: start deploy

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,5 @@
 import {defineRouting} from "next-intl/routing";
 import {locales, defaultLocale, localePrefix} from "./config";
-
 export const routing = defineRouting({
   locales,
   defaultLocale,
